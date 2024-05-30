d9947459a875c549d3a8a3fb977077f913c72698
Siena Schipke <sienaschipke@gmail.com>
2024-05-30 10:12:35 -0400

    initial commit with HTML and JS

---
+++ script.js
@@ -0,0 +1,16 @@
+// 
+// Allow user to input choice.
+
+function getUserChoice() {
+
+}
+
+
+// Check that input is R, P, or S - if not throw an error.
+// Create a function to randomize computer's choice.
+// Compare user and computer's choices to determine winner.
+// Announce winner.
+// Show total wins, ties, losses.
+// Show input choices.
+// Show replay button.
+// 
